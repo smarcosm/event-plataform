e7bf61c8ad12923a87dfb44d0ad7bf7d3e295167
SMarcosM <sebastiaomarcos49@gmail.com>
2022-07-05 11:23:17 -0300

    Creating Sidebar e Routes

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,5 +1,8 @@
 import { CheckCircle, Lock} from 'phosphor-react'
-import { isPast } from 'date-fns'
+import { isPast, format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR';
+
+
 interface LessonProps {
   title: string;
   slug: string;
@@ -9,10 +12,13 @@ interface LessonProps {
 
 export function Lesson(props: LessonProps) {
   const isLessonAvailable = isPast(props.availableAt);
+  const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
+    locale: ptBR,
+  })
   return(
     <a href="">
       <span className="text-gray-300">
-        {props.availableAt.toString()}
+        {availableDateFormatted}
       </span>
 
       <div className="rounded border border-gray-500 p-4 mt-2">
